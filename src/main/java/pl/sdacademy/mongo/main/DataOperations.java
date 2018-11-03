5405caa72dbada969db9f9fe03f5e6c049c2c73f
Sylwester Oleszek <sylwester.oleszek@gmail.com>
2018-11-03 11:47:51 +0100

    Update from ... to ...

--- src/main/java/pl/sdacademy/mongo/main/DataOperations.java
+++ src/main/java/pl/sdacademy/mongo/main/DataOperations.java
@@ -5,7 +5,9 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.MongoIterable;
 import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Updates;
 import org.bson.Document;
+import org.bson.types.ObjectId;
 import pl.sdacademy.mongo.utils.MongoUtils;
 
 import javax.print.Doc;
@@ -18,10 +20,17 @@ public class DataOperations {
     static MongoCollection<Document> collection = database.getCollection("wartości");
 
     public static void main(String[] args) {
-        insertData();
-        printData(10);
+        //insertData();
+        //printData(10);
         //findData("1d506545-f9e9-4711-b52c-9645dbe767b7");
-        findData(10, 10000);
+        //findData(10, 10000);
+        //findDataById("5bdd645df9f04849b4c771fd");
+        //update("5bdd645df9f04849b4c771fd", "mój tekst do aktualizacji");
+        //findDataById("5bdd645df9f04849b4c771fd");
+
+        findData(11, 12);
+        update(11, 12, "nowy tekst");
+        findData(11, 12);
     }
 
     private static void insertData() {
@@ -29,22 +38,22 @@ public class DataOperations {
         for (int i = 0; i < 100000; i++) {
             //Document musi być org.bson
             Document doc = new Document("text", UUID.randomUUID().toString())
-                    .append("value", (int)(Math.random() * 1000));
+                    .append("value", (int) (Math.random() * 1000));
             collection.insertOne(doc);
         }
     }
 
     private static void printData(int limit) {
         MongoIterable<Document> results = collection.find().limit(limit);
-        for(Document doc : results) {
+        for (Document doc : results) {
             System.out.println(doc.toJson());
         }
     }
 
-    private static void findData(String text){
+    private static void findData(String text) {
         System.out.println("Found: ");
         MongoIterable<Document> results = collection.find(Filters.eq("text", text));
-        for(Document doc : results) {
+        for (Document doc : results) {
             System.out.println(doc.toJson());
         }
 
@@ -59,8 +68,47 @@ public class DataOperations {
                         //less than equal
                         Filters.lte("value", rangeMax)
                 ));
-        for(Document doc : results) {
+        for (Document doc : results) {
             System.out.println(doc.toJson());
         }
+    }
+
+    private static void findDataById(String id) {
+        Document result = collection
+                .find(Filters.eq("_id", new ObjectId(id)))
+                .first();
+        if (result != null) {
+            System.out.println(result.toJson());
+        }
+    }
+
+    private static void update(String id, String text) {
+        System.out.println("Update id=" + id + " with value=" + text);
+        collection
+                .updateOne(
+                        Filters.eq("_id", new ObjectId(id)),
+                        Updates.set("text", text)
+                );
+    }
+
+    private static void update(Integer rangeMin, Integer rangeMax, String text) {
+        System.out.println("Updated in range (" + rangeMin + ", " + rangeMax + ")" + "with data " + text);
+        collection
+                .updateMany(
+                Filters.and(
+                        //greater than equal
+                        Filters.gte("value", rangeMin),
+                        //less than equal
+                        Filters.lte("value", rangeMax)),
+                Updates.set("text", text)
+                );
+    }
+
+    private static void deleteById(String id) {
+
+    }
+
+    private static void delete(Integer rangeMin, Integer rangeMax) {
+        
     }
 }
