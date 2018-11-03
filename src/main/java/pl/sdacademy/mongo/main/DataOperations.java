ad441f391ba6e03ca6ea50deef4dd1102aaee49e
Sylwester Oleszek <sylwester.oleszek@gmail.com>
2018-11-03 10:17:16 +0100

    InsertData i printData

--- src/main/java/pl/sdacademy/mongo/main/DataOperations.java
+++ src/main/java/pl/sdacademy/mongo/main/DataOperations.java
@@ -1,13 +1,39 @@
 package pl.sdacademy.mongo.main;
 
-public class DataOperations {
-    public static void main(String[] args) {
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoIterable;
+import org.bson.Document;
+import pl.sdacademy.mongo.utils.MongoUtils;
 
+import java.util.UUID;
+
+public class DataOperations {
+
+    static MongoClient client = MongoUtils.getInstance().getClient();
+    static MongoDatabase database = client.getDatabase("testy_nasze");
+    static MongoCollection<Document> collection = database.getCollection("wartości");
+
+    public static void main(String[] args) {
+        insertData();
+        printData(10);
     }
 
     private static void insertData() {
+
         for (int i = 0; i < 100; i++) {
-            
+            //Document musi być org.bson
+            Document doc = new Document("text", UUID.randomUUID().toString())
+                    .append("value", (int)Math.random() * 1000);
+            collection.insertOne(doc);
+        }
+    }
+
+    private static void printData(int limit) {
+        MongoIterable<Document> results = collection.find().limit(limit);
+        for(Document doc : results) {
+            System.out.println(doc.toJson());
         }
     }
 }
